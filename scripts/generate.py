3c81ece003ec57e5d20897f41e51740495bbccba
j1nma <j1nma@users.noreply.github.com>
2019-04-16 19:45:45 -0300

    static & dynamic particle generation

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,24 +1,22 @@
-from functions import is_int_string, generate_files
+from functions import is_int_string, generate_lennard_jones_gas_file
 import sys
 
-if len(sys.argv) != 8:
+if len(sys.argv) != 7:
     sys.exit("Arguments missing. Exit.")
 
-number_of_small_particles = sys.argv[1]
+number_of_particles = sys.argv[1]
 area_length = sys.argv[2]
-max_velocity_module = sys.argv[3]
+initial_speed = sys.argv[3]
 particle_radius = sys.argv[4]
 particle_mass = sys.argv[5]
-large_particle_radius = sys.argv[6]
-large_particle_mass = sys.argv[7]
+particle_bonding_energy = sys.argv[6]
 
-if not is_int_string(number_of_small_particles):
+if not is_int_string(number_of_particles):
     sys.exit("Must be integer. Exit.")
 
-generate_files(int(number_of_small_particles),
+generate_lennard_jones_gas_file(int(number_of_particles),
 float(area_length),
-float(max_velocity_module),
+float(initial_speed),
 float(particle_radius),
 float(particle_mass),
-float(large_particle_radius),
-float(large_particle_mass))
+float(particle_bonding_energy))
