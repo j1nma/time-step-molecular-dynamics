de261818ffecbd032d1ce7b22a5c7eed6475120b
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-04-19 21:02:57 -0300

    lennard jones gas comments

--- src/main/java/algorithms/TimeStepDrivenMolecularDynamics.java
+++ src/main/java/algorithms/TimeStepDrivenMolecularDynamics.java
@@ -22,5 +22,19 @@ public class TimeStepDrivenMolecularDynamics {
 			double initialVelocity) {
 
 		eventWriter.close();
+
+		System.out.println("Generar en java todas las particulas. no hace falta hacerlo en python. Recibir por" +
+				"parametro la cantidad y las dimensiones del lugar");
+
+		System.out.println("hacer el while y que en cada frame se calcule la posicion y velocidad de todas respecto de otras");
+
+		System.out.println("Es decir por cada particula calcular la sumatoria de todas las fuerzas de todas las particulas alrededor que le generan a esta.");
+
+		System.out.println("Para eso ultimo mirar verlet basico y usar ese.");
+
+		System.out.println("recordar que esto es por tiempo. no por eventos");
+
+		System.out.println("no c como manejar el caso de cuando choca contra la esquina cuando cambia de box....");
+		
 	}
 }
