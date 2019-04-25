d950cb9c81a04b5d766a75298371cb8c9f229674
j1nma <j1nma@users.noreply.github.com>
2019-04-24 21:11:14 -0300

    beeman and analytic solution with print example

--- src/main/java/io/OctaveWriter.java
+++ src/main/java/io/OctaveWriter.java
@@ -27,16 +27,34 @@ public class OctaveWriter {
 		this.fileWriter.close();
 	}
 
-	public void writeOrderValuesThroughIterations(final Stack<Double> orderValues) throws IOException { //TODO adapt
+	public void writePositionsThroughTime(final Stack<Double> timeStepValues,
+	                                      final Stack<Double> analyticValues,
+	                                      final Stack<Double> positionsValues) throws IOException { //TODO adapt
 		final StringBuilder builder = new StringBuilder();
-		builder.append("result = [").append(Double.toString(orderValues.pop()));
-		while (!orderValues.isEmpty()) {
-			builder.append(", ").append(Double.toString(orderValues.pop()));
+		builder.append("x = [").append(Double.toString(timeStepValues.pop()));
+		while (!timeStepValues.isEmpty()) {
+			builder.append(", ").append(Double.toString(timeStepValues.pop()));
 		}
 		builder.append("];").append("\n")
-				.append("plot(fliplr(result));").append("\n")
-				.append("xlabel(\"Iterations\");").append("\n")
-				.append("ylabel(\"Va\");").append("\n");
+				.append("y1 = [").append(Double.toString(analyticValues.pop()));
+		while (!analyticValues.isEmpty()) {
+			builder.append(", ").append(Double.toString(analyticValues.pop()));
+		}
+		builder.append("];").append("\n")
+				.append("y2 = [").append(Double.toString(positionsValues.pop()));
+		while (!positionsValues.isEmpty()) {
+			builder.append(", ").append(Double.toString(positionsValues.pop()));
+		}
+		builder.append("];").append("\n")
+				.append("plot(x, y1, \";Analítica;\");").append("\n")
+				.append("xlabel(\"Tiempo (s)\");").append("\n")
+				.append("ylabel(\"Posición (m)\");").append("\n")
+				.append("set(gca, 'ytick', -1:0.2:1);").append("\n")
+				.append("hold all").append("\n")
+				.append("plot(x, y2, \";Beeman;\", \"color\", 'g','LineStyle','--');").append("\n")
+				.append("print(\"./output/beeman/beeman.svg\", \"-dsvg\", \"-F:12\")").append("\n")
+		;
+
 		fileWriter.append(builder.toString());
 		fileWriter.flush();
 	}
