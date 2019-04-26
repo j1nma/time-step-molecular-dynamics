50f50cceeb993faebfc8fe4060e5200c24d4586a
j1nma <j1nma@users.noreply.github.com>
2019-04-25 21:32:30 -0300

    adapted App for TimeStep

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,15 @@
 import algorithms.TimeStepDrivenMolecularDynamics;
 import com.google.devtools.common.options.OptionsParser;
-import io.Parser;
 import io.SimulationOptions;
-import models.Particle;
 
 import java.io.File;
-import java.io.PrintWriter;
 import java.util.Collections;
-import java.util.List;
 
 public class App {
 
 	private static final String OUTPUT_DIRECTORY = "./output";
 	private static final String EX_1_DIRECTORY = OUTPUT_DIRECTORY + "/ex1";
 	private static final String POSITIONS_PLOT_FILE = EX_1_DIRECTORY + "/positions.svg";
-
-	private static final String OVITO_FILE = OUTPUT_DIRECTORY + "/ovito_file.txt";
-	private static PrintWriter eventWriter;
 
 	public static void main(String[] args) {
 
@@ -32,69 +25,21 @@ public class App {
 		if (options.limitTime <= 0
 				|| options.deltaT <= 0
 				|| options.printDeltaT <= 0
-				|| options.mass <= 0
-				|| options.staticFile.isEmpty()
-				|| options.dynamicFile.isEmpty()) {
+				|| options.mass <= 0) {
 			printUsage(parser);
 		}
 
-		if (options.lennardJonesGas) {
-			if (options.staticFile.isEmpty() || options.dynamicFile.isEmpty())
-				printUsage(parser);
-
-			// Parse static and dynamic files
-			Parser staticAndDynamicParser = new Parser(options.staticFile, options.dynamicFile);
-			if (!staticAndDynamicParser.parse()) return;
-			List<Particle> particles = staticAndDynamicParser.getParticles();
-		}
-
-
-		// Initialize file writers
-//		eventWriter = new PrintWriter(new FileWriter("TODO GAS FILE")); //TODO CORRECT FILE NAME
-
 		// Run algorithm
-		runAlgorithm(
+		TimeStepDrivenMolecularDynamics.run(
 				options.limitTime,
 				options.deltaT,
 				options.printDeltaT,
 				options.k,
 				options.vdc,
 				options.initialPosition,
-				options.mass
-		);
-	}
-
-	private static void runAlgorithm(double limitTime,
-	                                 double deltaT,
-	                                 double printDeltaT,
-	                                 double k,
-	                                 double vdc,
-	                                 double initialPosition,
-	                                 double mass) {
-
-		StringBuffer buffer = new StringBuffer();
-
-		TimeStepDrivenMolecularDynamics.run(
-				buffer,
-				eventWriter,
-				limitTime,
-				deltaT,
-				printDeltaT,
-				k,
-				vdc,
-				initialPosition,
-				mass,
+				options.mass,
 				POSITIONS_PLOT_FILE
 		);
-
-//		OvitoWriter<Particle> ovitoWriter;
-//		try {
-//			ovitoWriter = new OvitoWriter<>(Paths.get(OVITO_FILE));
-//			ovitoWriter.writeBuffer(buffer);
-//			ovitoWriter.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
 	}
 
 	private static void printUsage(OptionsParser parser) {
