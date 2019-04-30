4b0aa91fd15bec71acb4fb7dc2db4e84727343ac
j1nma <j1nma@users.noreply.github.com>
2019-04-29 23:38:47 -0300

    generate: radius = 0.0, mass = 1.0

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,14 +1,12 @@
 from functions import is_int_string, generate_lennard_jones_gas_file
 import sys
 
-if len(sys.argv) != 6:
+if len(sys.argv) != 4:
     sys.exit("Arguments missing. Exit.")
 
 number_of_particles = sys.argv[1]
 area_length = sys.argv[2]
 initial_speed = sys.argv[3]
-particle_radius = sys.argv[4]
-particle_mass = sys.argv[5]
 
 if not is_int_string(number_of_particles):
     sys.exit("Must be integer. Exit.")
@@ -16,5 +14,5 @@ if not is_int_string(number_of_particles):
 generate_lennard_jones_gas_file(int(number_of_particles),
 float(area_length),
 float(initial_speed),
-float(particle_radius),
-float(particle_mass))
+float(0),
+float(1))
--- src/main/java/io/Parser.java
+++ src/main/java/io/Parser.java
@@ -38,9 +38,9 @@ public class Parser {
 		}
 		numberOfParticles = sc.nextInt();
 		for (int i = 0; i < numberOfParticles; i++) {
-			double radius = sc.nextDouble();
+			double radius = sc.nextDouble(); // ignore radius, since particles are punctual (r = 0)
 			double mass = sc.nextDouble();
-			particles.add(new Particle(i + 1, radius, mass));
+			particles.add(new Particle(i + 1, mass));
 		}
 		sc.close();
 		return true;
