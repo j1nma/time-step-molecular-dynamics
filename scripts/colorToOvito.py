794f60c3f86eff27de2aeb5e93c491c85feacbf3
j1nma <j1nma@users.noreply.github.com>
2019-06-08 01:20:26 -0300

    colorToOvito

---
+++ scripts/colorToOvito.py
@@ -0,0 +1,34 @@
+import math  
+
+data = open("./output/ovito_file.txt", "r").readlines()
+N = int(data[0])
+
+output = []
+
+# Delete first N line
+output.append(data[0].rstrip())
+del data[0]
+
+# Delete first t line
+output.append(data[0].rstrip())
+del data[0]
+
+c = 0
+for line in data:
+	line = line.rstrip()
+	xvs = line.split()
+	vx = float(xvs[3])
+	vy = float(xvs[4])
+	v = math.sqrt(vx*vx + vy*vy)
+	line = "%s %s" % (line, v)
+	output.append(line)
+	# output = output + line
+	c = c + 1
+	if (c % N == 0) and (c != len(data)):
+		output.append(data[c].rstrip())
+		del data[c]
+		output.append(data[c].rstrip())
+		del data[c]
+f = open("./output/color_ovito_file.txt", "w")
+f.write("\n".join(output))
+f.close()
