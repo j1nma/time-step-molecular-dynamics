32cc9715b1233e7d898ae2888dcd5c9c0868626b
j1nma <j1nma@users.noreply.github.com>
2019-06-13 16:51:42 -0300

    energy(N, dt)

--- scripts/energy.m
+++ scripts/energy.m
@@ -2,7 +2,7 @@ function energy(N, dt)
     disp(sprintf("./output/ex2/N=%d/energy_dT=%s.txt", N, dt))
     fid = fopen(sprintf("./output/ex2/N=%d/energy_dT=%s.txt", N, dt));
 
-    energy = [0.0];
+    energy = [];
     # Read file
     while (!feof(fid))
             % Parse out energy
@@ -11,13 +11,22 @@ function energy(N, dt)
 
     fclose(fid);
 
+    print_dt = 0.016666667;
+
     props = {"marker", '.', 'LineStyle', 'none'};
-    h = plot((1:size(energy, 2)), energy, sprintf(";dT = %s;", dt));
-    set(h, props{:})
-    xlabel("something");
-    ylabel("Energía");
+    %log10(energy / energy(1))
+    h = plot((1:size(energy, 2)) * print_dt, energy / energy(1), sprintf(";dT = %s s;", dt));
+    %set(h, props{:})
+    xlabel("Tiempo [s]");
+    ylabel("Energía Total_{t} / Energía_{0} [J]");
+    xlim([0, 5])
     legend("location", "eastoutside");
     grid on
+
+    mean(energy / energy(1))
+    std(energy / energy(1))
+
+    hold all
 
     print(sprintf("%s/N=%d/energy-dT=%s.png", './output/ex2', N, dt), "-dpngcairo", "-F:12")
 end
