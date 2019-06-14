ef253b1fb1b605c664c95dd34c3af40d1ba76697
j1nma <j1nma@users.noreply.github.com>
2019-06-14 03:58:52 -0300

    graph speeds

---
+++ scripts/graphSpeedsPDF.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import numpy
+from oct2py import octave
+octave.addpath('./scripts/')
+
+N = 1000
+dt = "0.00125"
+ratio = "0.75"
+initial_ratio = "0.33"
+func = 'lastThirdSpeedsPDF(' + str(N) + ',\"' + str(dt) + '\",\"' + str(ratio) + '\",\"' + str(initial_ratio) + '\")';
+octave.eval(func);
