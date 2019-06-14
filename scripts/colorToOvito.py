8c00a65128a84d3c1eeb4b58fb584bd3de441acb
j1nma <j1nma@users.noreply.github.com>
2019-06-14 00:49:22 -0300

    color to ovito parameters

--- scripts/colorToOvito.py
+++ scripts/colorToOvito.py
@@ -3,28 +3,29 @@ import sys
 
 N = sys.argv[1]
 dt = sys.argv[2]
+ratio_N = int(sys.argv[3])
+ratio_D = int(sys.argv[4])
+verbose = int(sys.argv[5])
 
 data0 = open("./output/ex2/N={N}/ovito_file_dT={dt}.txt".format(N = N, dt = dt), "r").readlines()
 N = int(data0[0])
 
 initial_size = len(data0)
 
-print(initial_size)
-
-cut = (initial_size*3//4)
-
-print(cut)
+cut = (initial_size*ratio_N // ratio_D)
 
 i = 0
-for line in data0[cut:]:
-    line = line.rstrip()
-    if (line == str(N)):
-        break;
-    i = i + 1
+if cut != 0:
+    for line in data0[cut:]:
+       line = line.rstrip()
+       if (line == str(N)):
+           break;
+       i = i + 1
 
-print(i)
-
-print(data0[cut + i])
+if verbose == 1:
+    print('initial_size: {initial_size}'.format(initial_size = initial_size))
+    print('cut index: {cut}'.format(cut = cut))
+    print('real cut index: {cut}'.format(cut = cut+i))
 
 data = data0[cut+i:]
 
@@ -47,14 +48,14 @@ for line in data:
 	v = math.sqrt(vx*vx + vy*vy)
 	line = "%s %s" % (line, v)
 	output.append(line)
-	# output = output + line
 	c = c + 1
-	print("Progress: {p}".format(p = 100 * (c/(initial_size-cut))))
 	if (c % N == 0) and (c != len(data)):
-		output.append(data[c].rstrip())
-		del data[c]
-		output.append(data[c].rstrip())
-		del data[c]
+	    output.append(data[c].rstrip())
+	    del data[c]
+	    output.append(data[c].rstrip())
+	    del data[c]
+	if verbose == 1 and (c % N == 0):
+	    print("Progress: {p}".format(p = 100 * (c/len(data))))
 f = open("./output/ex2/N={N}/color_ovito_file_dT={dt}.txt".format(N = N, dt = dt), "w")
 f.write("\n".join(output))
 f.close()
